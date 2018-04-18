ac5741c002ca07658c26e7f1d78fb51b861c7a3d
arivacca <ariannav14@gmail.com>
2018-04-18 00:45:14 -0600

    Coltie larsiegit add src/git add src/git add src/

--- src/main/java/cs455/jobData/WordCount.java
+++ src/main/java/cs455/jobData/WordCount.java
@@ -10,17 +10,20 @@ import scala.Tuple2;
 import java.io.IOException;
 import java.util.List;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
 
-public class WordCount{
-    public static void main(String[] args) throws IOException {
-        SparkConf sparkConf = new SparkConf().setAppName("cs455-hw3-spark-example");
-        JavaSparkContext sc = new JavaSparkContext(sparkConf);
-        Configuration hadoopConfiguration = sc.hadoopConfiguration();
-        JavaRDD<String> textFile = sc.textFile("hdfs://TP/input.txt");
-        JavaPairRDD<String, Integer> counts = textFile
-            .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
-            .mapToPair(word -> new Tuple2<>(word, 1))
-            .reduceByKey((a, b) -> a + b);
-        counts.saveAsTextFile("hdfs://TP/output.txt");
-    }
+public class WordCount {
+  public static void main(String[] args) {
+    String logFile = "hdfs://santa-fe:48800/TP/input.txt"; // Should be some file on your system
+    SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+    Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+    long numAs = logData.filter(s -> s.contains("a")).count();
+    long numBs = logData.filter(s -> s.contains("b")).count();
+
+    System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+    spark.stop();
+  }
 }
