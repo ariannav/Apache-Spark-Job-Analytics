7df619810674a431e099470a0d8b03ad213bd76c
Colton Larson <cwlarson@rams.colostate.edu>
2018-04-19 16:10:00 -0600

    Add files via upload

---
+++ src/main/java/cs455/jobData/JobSkills.java
@@ -0,0 +1,43 @@
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import scala.Tuple2;
+import scala.collection.JavaConverters;
+
+// import au.com.bytecode.opencsv.CSVReader;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.*;
+
+public class JobSkills{
+    
+    public static void main(String[] args) throws Exception {
+    // Try reading in all the json files
+		String filePath = "hdfs://little-rock:46601/cs455/TP/data/*.json";
+		String[] jsonFiles = filePath.split(",");
+		SparkSession session = SparkSession.builder().getOrCreate();
+		Dataset<Row> jobsDataSet = session.read().json(jsonFiles);
+		Dataset<Row> reducedDataSet = 
+            jobsDataSet.drop("positionPeriod").drop("hiringOrganization").drop("normalizedTitle").drop("baseSalary").drop("jobLocation").drop("dateExpires").drop("employmentType").drop("id").drop("incentiveCompensation").drop("jobBenefits").drop("numberOfOpenings").drop("salaryCurrency").drop("specialCommitments").drop("title").drop("url").drop("veteranCommitment").drop("workHours");
+        reducedDataSet.show();
+        reducedDataSet.printSchema();
+        Dataset<Row> skillsDataSet = reducedDataSet.select("skills");
+        skillsDataSet.show();
+//         reducedDataSet.createOrReplaceTempView("jobs");
+//         reducedDataSet.sql("SELECT skills FROM jobs WHERE skills IS NOT NULL").show();
+        //JavaRDD<Row> rdd = reducedDataSet.rdd().toJavaRDD();
+        //rdd.saveAsTextFile("hdfs://little-rock:46601/cs455/TP/output/output");
+    }
+}
